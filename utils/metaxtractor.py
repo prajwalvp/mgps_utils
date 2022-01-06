31ae35b6bd256f751aa23aef26d3ac228b67e2ff
Prajwal Padmanabh <prajwal3108@gmail.com>
2022-01-06 02:49:13 +0100

    Compatible for PMPS/HTRU csv file

--- utils/metaxtractor.py
+++ utils/metaxtractor.py
@@ -455,6 +455,9 @@ def generate_info_from_meta(opts):
         gls = unpublished_df['gl (deg) ']
         gbs = unpublished_df['gb (deg) ']
         unpublished_psr_coords = SkyCoord(gls*u.deg, gbs*u.deg, frame='galactic').transform_to('icrs')
+        #ras = unpublished_df['RA(deg)']
+        #decs = unpublished_df['DEC(deg)']
+        #unpublished_psr_coords = SkyCoord(ras*u.hourangle, decs*u.deg, frame='icrs')
         max_radius = survey_beam_radius + 0.1166 # 7 arcmin for L-band at Parkes
         unpublished_cnt =0
 

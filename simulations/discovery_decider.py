a23d21897b34693c156be80a86607b9a5a5955b2
prajwalvp <prajwal3108@gmail.com>
2022-01-20 23:18:20 +0000

    Working version based on 50 percent ellipse region thresholding

--- simulations/discovery_decider.py
+++ simulations/discovery_decider.py
@@ -1,14 +1,25 @@
+import math
 import subprocess
 import numpy as np
+from astropy import wcs
+from astropy.time import Time
 from astropy.io import fits
 import astropy.units as u
 from astropy.coordinates import SkyCoord, match_coordinates_sky
+from ast import literal_eval
+import matplotlib.pyplot as plt
+from matplotlib.patches import Ellipse
 
 """
 Map psr coordinate to gain factor via actual MGPS psf and decide if survey actually capable of discovering the pulsar 
 """
 
-def convert_equatorial_coordinate_to_pixel(equatorial_coordinates, bore_sight, utc_time):
+
+ALL_META_PATH = '/MGPS/survey_stats/all_metafiles'
+SURVEY_BEAM_RADIUS = 1.1*0.2488 # 10 percent tolerance over the actual value
+
+
+def convert_equatorial_coordinate_to_pixel(equatorial_coordinates, bore_sight):
     """
     Credits: Weiwei's Mosaic
     https://docs.astropy.org/en/stable/wcs/index.html#using-astropy-wcs
@@ -26,6 +37,22 @@ def convert_equatorial_coordinate_to_pixel(equatorial_coordinates, bore_sight, u
     pixel_coordinates = scaled_pixel_coordinates*step 
      
     return pixel_coordinates
+
+
+def get_pixel_coherent_beam_coordinates(beam_coords, boresight_coords):
+    """
+    Convert coherent beam equatorial coordinates to pixel coordinates
+    """
+    pixel_beam_ras=[]
+    pixel_beam_decs=[]
+    # Convert equatorial beam coordinates to pixel coordinates
+    for beam_coord in beam_coords:
+        pixel_coordinates = convert_equatorial_coordinate_to_pixel(beam_coord, boresight_coords)
+        pixel_beam_ras.append(boresight_coords.ra.deg + pixel_coordinates[0][0])
+        pixel_beam_decs.append(boresight_coords.dec.deg + pixel_coordinates[0][1])
+
+    return pixel_beam_ras, pixel_beam_decs
+
 
 
 def get_coherent_beam_coords(meta):
@@ -53,16 +80,120 @@ def get_coherent_beam_coords(meta):
         coherent_beams_ra.append(vals[i].split(',')[-2])
         coherent_beams_dec.append(vals[i].split(',')[-1])
     
-    # Convert equatorial beam coordinates to pixel coordinates
     beam_coords = SkyCoord(frame='icrs', ra= coherent_beams_ra, dec= coherent_beams_dec, unit=(u.hour, u.deg))
 
     return beam_coords
 
 
-ALL_META_PATH = '/MGPS/survey_stats/all_metafiles'
-SURVEY_BEAM_RADIUS = 1.1*0.2488 # 10 percent tolerance over the actual value
+def pointInEllipse(x,y,xp,yp,d,D,angle):
+    """
+    tests if a point[xp,yp] is within
+    boundaries defined by the ellipse
+    of center[x,y], diameter d D, and tilted at angle
+    """
+
+    cosa=math.cos(angle)
+    sina=math.sin(angle)
+    dd=d/2*d/2
+    DD=D/2*D/2
+
+    a = math.pow(cosa*(xp-x)+sina*(yp-y),2)
+    b = math.pow(sina*(xp-x)-cosa*(yp-y),2)
+    ellipse=(a/dd)+(b/DD)
+    if ellipse <= 1:
+        return True
+    else:
+        return False
 
 
+def check_discovery(meta, psr):
+
+    with open(meta, 'r') as f:
+        data = f.read()
+    all_info = literal_eval(data)
+
+    
+    print "Meta file of interest:%s"%(meta)
+    # Initialise plot
+    plt.clf()
+    fig, ax = plt.subplots()
+    ax.set_aspect(1)
+
+    # utc time for sign convention issue
+    utc_time = all_info['utc_start'].replace(" ","T").replace("/","-")
+    print "UTC time: %s"%utc_time
+
+    # Pointing name
+    pointing_name = all_info['boresight'].split(',')[0]
+    print "Pointing of interest: %s"%pointing_name
+
+    # Boresight coordinates
+    boresight_ra = all_info['boresight'].split(',')[-2]
+    boresight_dec = all_info['boresight'].split(',')[-1]
+    boresight_coords = SkyCoord(frame='icrs', ra=boresight_ra, dec=boresight_dec, unit=(u.hour, u.deg))
+    boresight_ra_deg = boresight_coords.ra.value
+    boresight_dec_deg = boresight_coords.dec.value
+
+    # Get all coherent beam coords
+    coherent_beam_coords = get_coherent_beam_coords(meta)
+
+    # Beam shapes
+    beam_width = 2.0*float(literal_eval(all_info['beamshape'])['x'])
+    beam_height = 2.0*float(literal_eval(all_info['beamshape'])['y'])
+    if utc_time < Time('2021-10-05T00:00:00'):
+        beam_angle =  float(literal_eval(all_info['beamshape'])['angle'])
+    else:
+        beam_angle = -1.0*float(literal_eval(all_info['beamshape'])['angle']) # Sign change in FBFUSE tiling rollout   
+
+
+    # Plot psr in pixel coordinates
+    psr_coords = SkyCoord(frame='icrs', ra=psr[0], dec=psr[1], unit=(u.deg, u.deg))
+    psr_pixel_coordinates = convert_equatorial_coordinate_to_pixel(psr_coords, boresight_coords)
+    psr_pixel_ra = boresight_ra_deg + psr_pixel_coordinates[0][0]
+    psr_pixel_dec = boresight_dec_deg + psr_pixel_coordinates[0][1]
+    psr_pixel_coords = SkyCoord(frame='icrs', ra=psr_pixel_ra, dec = psr_pixel_dec, unit=(u.deg, u.deg))
+    ax.plot(psr_pixel_ra, psr_pixel_dec,'*',label='psr',markersize=7.5)
+
+
+    #Plot all ellipses
+    
+
+
+    # Add best ellipse
+    pixel_beam_ras, pixel_beam_decs = get_pixel_coherent_beam_coordinates(coherent_beam_coords, boresight_coords)
+    psr_idx, psr_d2d, psr_d3d = psr_coords.match_to_catalog_sky(coherent_beam_coords)
+    best_ellipse = Ellipse(xy=(pixel_beam_ras[psr_idx], pixel_beam_decs[psr_idx]), width=beam_width, height=beam_height,angle = beam_angle, edgecolor='blue', fc='grey', lw=1.5)
+    ax.add_patch(best_ellipse)
+
+    best_beam = 'beam_%d'%(psr_idx)
+    print "Best beam: %s"%(best_beam)
+    # Return closest 3 beams
+    all_seps = psr_coords.separation(coherent_beam_coords)
+    all_beams_sorted = np.argsort(all_seps)
+    ellipse_2 = Ellipse(xy=(pixel_beam_ras[all_beams_sorted[1]], pixel_beam_decs[all_beams_sorted[1]]), width=beam_width, height=beam_height,angle = beam_angle, edgecolor='blue', lw=1.5) 
+    ellipse_3 = Ellipse(xy=(pixel_beam_ras[all_beams_sorted[2]], pixel_beam_decs[all_beams_sorted[2]]), width=beam_width, height=beam_height,angle = beam_angle, edgecolor='blue', lw=1.5) 
+    ax.add_patch(ellipse_2)
+    ax.add_patch(ellipse_3)
+
+    ax.set_xlabel('Right Ascension (Degrees)')
+    ax.set_ylabel('Declination (Degrees)')
+
+    ax.set_xlim(psr_pixel_ra - 3*beam_width, psr_pixel_ra + 3*beam_width)
+    ax.set_ylim(psr_pixel_dec - 3*beam_width, psr_pixel_dec + 3*beam_width)
+
+    plt.savefig('%s_%s.png'%(utc_time, best_beam), dpi=300)
+
+    # Return true if the pulsar is in any of the 3 closest beams
+       
+    if pointInEllipse(pixel_beam_ras[psr_idx], pixel_beam_decs[psr_idx], psr_pixel_ra, psr_pixel_dec, beam_width, beam_height, beam_angle):
+       return True
+    if pointInEllipse(pixel_beam_ras[all_beams_sorted[1]], pixel_beam_decs[all_beams_sorted[1]], psr_pixel_ra, psr_pixel_dec, beam_width, beam_height, beam_angle):
+       return True
+    if pointInEllipse(pixel_beam_ras[all_beams_sorted[2]], pixel_beam_decs[all_beams_sorted[2]], psr_pixel_ra, psr_pixel_dec, beam_width, beam_height, beam_angle):
+       return True
+    return False    
+
+    
 cnt=0
 # Read MGPS pointing coordinates
 pointings = np.loadtxt('MGPS-L_all_pointing_coordinates.txt', dtype=str)[:,0]
@@ -96,8 +227,9 @@ for i in range(len(psr_discovery_coords)):
         cnt+=1
 print "Number of remaining discoveries based on survey beam radius cutoff: %d"%(cnt)
 
+true_discoveries=[]
 
-# Map pointing to meta file and get closest beam position + separation
+# Map pointing to meta file and get closest beam position + orientation to decide if psr within beam
 for psr in psr_remaining:
     pointing = psr[2]
  
@@ -109,17 +241,25 @@ for psr in psr_remaining:
         continue
     
     if len(meta_path.decode('utf-8').split('\n')) > 2:
-        print "Observed multiple times"
+        print "Observed multiple times..."
         first_meta_file = meta_path.decode('utf-8').split('\n')[0]      
-         second_meta_file = meta_path.decode('utf-8').split('\n')[1]
-        print (first_meta_file, second_meta_file)
-        with open(first_meta_file, 'r') as f:
-            data = f.read()
-        all_info = literal_eval(data)
-        first = all_info['output_dir']
-        with open(second_meta_file, 'r') as f:
-            data = f.read()
-        all_info = literal_eval(data)
-        second = all_info['output_dir']
+        second_meta_file = meta_path.decode('utf-8').split('\n')[1]
+        print first_meta_file, second_meta_file
+
+        # Check if pulsars falls within any of the beams from either epochs
+        if check_discovery(first_meta_file, psr): 
+            true_discoveries.append(psr)
+                
+        if check_discovery(second_meta_file, psr): 
+           true_discoveries.append(psr)  
+
+    else: 
+        print "Observed once..."    
+        meta_file = meta_path.decode('utf-8').split('\n')[0]        
+        if check_discovery(meta_file, psr):
+           print "Disovery is found by synthesis beams and considered true. Will be retained"  
+           true_discoveries.append(psr)  
+        else:
+           print "Discovery outside the coherent beam region. Will be ignored" 
 
 
